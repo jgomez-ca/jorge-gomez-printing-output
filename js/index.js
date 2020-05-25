623ee070413b429039e550648d2f9dfe9ffde0b0
jgomezt <59587212+jogomezt@users.noreply.github.com>
2020-05-25 10:00:08 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,65 @@
+let a = 3;
+let b = 5;
+let c;
+
+let output;
+output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '----------\n';
+output += 'a + b = ' + (a + b) + '\n';
+output += 'a - b = ' + (a - b) + '\n';
+output += 'a * b = ' + (a * b) + '\n';
+output += 'a / b = ' + (a / b) + '\n';
+output += 'a % b = ' + (a % b) + '\n';
+output += 'a += b = ' + (a += b) + '\n';
+output += 'a -= b = ' + (a -= b) + '\n';
+output += 'a *= b = ' + (a *= b) + '\n';
+output += 'a /= b = ' + (a /= b) + '\n';
+output += 'a %= b = ' + (a %= b) + '\n';
+output += 'a == b = ' + (a == b) + '\n';
+output += 'a != b = ' + (a != b) + '\n';
+output += 'a > b = ' + (a > b) + '\n';
+output += 'a < b = ' + (a < b) + '\n';
+output += '!a && !c = ' + (!a && !c) + '\n';
+output += '!a || !c = ' + (!a || !c) + '\n';
+
+alert( output );
+
+//a + b
+//a - b
+//a * b
+//a / b
+//a % b
+//a += b
+//a -= b
+//a *= b
+//a /= b
+//a %= b
+//a == b
+//a != b
+//a > b
+//a < b
+//!a && !c 
+//!a || !c 
+
+
+alerted_value + 'a - b \n' + 'a * b \n' + 'a / b \n' + 'a % b \n' + 'a += b \n' + 'a -= b \n' + 'a *= b \n' + 'a /= b \n' + 'a %= b \n' + 'a == b \n' + 'a != b \n' + 'a > b \n' + 'a < b \n' + '!a && !c \n' + '!a || !c \n';
+
+console.log('a + b ${a+b}\n' + 'a - b \n' + 'a * b \n' + 'a / b \n' + 'a % b \n' + 'a += b \n' + 'a -= b \n' + 'a *= b \n' + 'a /= b \n' + 'a %= b \n' + 'a == b \n' + 'a != b \n' + 'a > b \n' + 'a < b \n' + '!a && !c \n' + '!a || !c \n');
+	
+	
+//alert("description of alerted_value: \n" + alerted_value );
+
+
+
+//Task 2
+//Declare the variable first_name and assign it with the value of your first name;
+//Declare the variable last_name and assign it with the value of your last name;
+//Declare the variable email and assign it with the value of your email;
+//Declare the variable output;
+//Create the expression concatenating variables first_name, last_name, email and string literals needed to complete the sentence reading: 'My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.';
+//Assign output with gotten expression;
+//Print the output in JavaScript console.
+
+
